0b0729541ed44f70cc2fc13b123e51e148f150e5
Amrelmalky <69693889+Amrelmalky@users.noreply.github.com>
2022-11-23 17:48:38 +0200

    Updates

---
+++ precache-manifest.f9a93aab4d034f46e2a77f2fe4be43b8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "127acfa9d949e766e482a74ee9790203",
+    "url": "/menus/index.html"
+  },
+  {
+    "revision": "e35e856a814bb9cea19a",
+    "url": "/menus/static/css/main.d93157bb.chunk.css"
+  },
+  {
+    "revision": "4642341b78d581a3b872",
+    "url": "/menus/static/js/2.ce02c916.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/menus/static/js/2.ce02c916.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e35e856a814bb9cea19a",
+    "url": "/menus/static/js/main.64c5d8f6.chunk.js"
+  },
+  {
+    "revision": "0f49ffef3085897d8005",
+    "url": "/menus/static/js/runtime-main.0c6c3e74.js"
+  }
+]);
